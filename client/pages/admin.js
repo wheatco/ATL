5e69a3d632ab5e664bed0829ebd4685f694e8815
David Pickart <pickartd@carleton.edu>
2016-06-29 00:13:15 -0400

    Tool mgmt pt 1

--- client/pages/admin.js
+++ client/pages/admin.js
@@ -1,5 +1,33 @@
 // Mithril component for tool management and maybe other things
 
+var Checklist = {
+  vm: {},
+  controller: function (args) {
+    var vm = Checklist.vm;
+    if (args.items) vm.items = args.items().data;
+  },
+  view: function (ctrl, args) {
+    var vm = Checklist.vm;
+    return m('.calc-item', vm.items.map(function(item) {
+        return m('label.checkbox', {
+            // class: valProp() == item.val ? 'active' : ''
+        }, [
+            m('button', {
+                // type: 'radio',
+                // checked: valProp() == item.val,
+                // value: item.val,
+                // onclick: runCallbacks(customOnClick)
+            }, 'x'),
+            m('div.checkbox-label', [
+                m('strong', item.name || ''),
+                // m('span.hint', item.hint || ''),
+            ])
+        ]);
+    }));
+    // return m("p", vm.data[0]);
+  }
+}
+
 var AdminPage = {};
 
 //for simplicity, we use this component to namespace the model classes
@@ -18,7 +46,11 @@ AdminPage.view = function(ctrl, args) {
 
     return m("div", [
         m('h1.title', 'Admin Page'),
-        m('p', 'Nothing here yet...')
+        m('.calc.row.center.gap-5', [
+          m('div.fill', [
+            m.component(Checklist, {items: vm.tools})
+            ])
+        ])
     ]);
 }
 
