8cf9bf77fdec825944269107111d4c0c39fe511c
Adam Canady <adam@adamcanady.com>
2016-07-08 14:23:45 -0700

    Make deploy work

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh root@atl.staging.wheat.co -c "cd ATL && git pull origin master && npm install && forever restartall"
+ssh root@atl.staging.wheat.co "cd ATL && git pull origin master && npm install && forever restartall"
