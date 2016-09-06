06c23ffb4a59b21510916cb0d3d87857f655abbf
JSlote <joseph.slote@gmail.com>
2016-09-05 23:13:14 -0500

    fixeD

--- src/middleware/renderPDF.js
+++ src/middleware/renderPDF.js
@@ -5,7 +5,7 @@ const wkhtmltopdf = require('wkhtmltopdf');
 module.exports = function(app) {
   return function(req, res, next) {
     if (req.query.view == 'pdf') {
-      wkhtmltopdf(res.data, { pageSize: 'letter' })
+      wkhtmltopdf(res.data, { pageSize: 'letter' , 	marginTop: 0, marginLeft: 0, marginRight: 0, marginBottom: 0})
         .pipe(res);
     } else next();
   };
