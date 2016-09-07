f10b926fbeeca7a28cfac8dc306d26151c02f181
JSlote <joseph.slote@gmail.com>
2016-09-06 22:27:46 -0500

    add 'digital quote'

--- src/middleware/sendQuoteEmail.js
+++ src/middleware/sendQuoteEmail.js
@@ -30,7 +30,7 @@ module.exports = function(app) {
           from: 'American Tape and Label <postmaster@sandbox90234bbf28114475b4475f6df62e30a1.mailgun.org>',
           to: quote.email,
           subject: 'Your Quote From ATL',
-          html: `Dear ${quote.name},<br>Please see attached for your quote created ${formatDate(quote.createdAt)}.<br><br>Thanks,<br>The American Tape & Label team<br>`,
+          html: `Dear ${quote.name},<br>Please see attached for your digital label quote created ${formatDate(quote.createdAt)}.<br><br>Thanks,<br>The American Tape & Label team<br>`,
           attachment: 'views/pdfs/quote.pdf'
         };
         mailgun.messages().send(mailData, function(err, body) {
